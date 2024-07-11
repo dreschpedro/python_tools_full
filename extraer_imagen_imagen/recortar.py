7ea7a6cb768f2615ed3aef2b60814d13965fcd72
dreschpedro <99pedrodresch@gmail.com>
2024-07-11 16:54:39 -0300

    recorte de imagenes para choco

--- extraer_imagen_imagen/recortar.py
+++ extraer_imagen_imagen/recortar.py
@@ -5,10 +5,40 @@ import numpy as np
 
 # Definir las coordenadas de los recortes (ajustar según sea necesario)
 # Estas coordenadas son (left, upper, right, lower)
+# coords = [
+#     (0, 630, 1400, 1730),   # Coordenadas de la primera imagen
+#     (0, 2400, 1400, 3405)   # Coordenadas de la segunda imagen
+# ]
+
 coords = [
-    (415, 130, 2275, 1730),   # Coordenadas de la primera imagen
-    (415, 1775, 2275, 3375)   # Coordenadas de la segunda imagen
+    (110, 465, 750, 1010),   #1 imagen
+    (110, 1455, 750, 2000),   #2 imagen
+    (110, 2530, 750, 3075),   #3 imagen
+
+    (930, 465, 1570, 1010),   #4 imagen
+    (930, 1455, 1570, 2000),   #5 imagen
+    (930, 2530, 1570, 3075),   #6 imagen
+
+    (1750, 465, 2390, 1010),   #7 imagen
+    (1750, 1455, 2390, 2000),  #8 imagen
+    (1750, 2530, 2390, 3075),  #9 imagen
 ]
+
+# +---------+---------+---------+
+# |         |         |         |
+# |   I1    |   I4    |   I7    |
+# |         |         |         |
+# +---------+---------+---------+
+# |         |         |         |
+# |   I2    |   I5    |   I8    |
+# |         |         |         |
+# +---------+---------+---------+
+# |         |         |         |
+# |   I3    |   I6    |   I9    |
+# |         |         |         |
+# +---------+---------+---------+
+
+
 
 # Obtener la lista de todas las imágenes en la carpeta
 image_folder = "./images"  # Cambiar esto al nombre de tu carpeta
--- extraer_imagen_imagen/search.py
+++ extraer_imagen_imagen/search.py
@@ -2,7 +2,7 @@ from PIL import Image
 import numpy as np
 
 # Cargar la imagen
-image_path = "imagen20.jpg"
+image_path = "imagen.jpg"
 image = Image.open(image_path)
 
 # Convertir a array de numpy para facilitar la manipulación
@@ -10,10 +10,35 @@ image_array = np.array(image)
 
 # Definir las coordenadas de los recortes (aquí tendrás que ajustar manualmente)
 # Estas coordenadas son (left, upper, right, lower)
+
 coords = [
-    (390, 130, 2280, 1730),   # Coordenadas de la primera imagen
-    (390, 1775, 2280, 3375)   # Coordenadas de la segunda imagen
+    (110, 465, 750, 1010),   #1 imagen
+    (110, 1455, 750, 2000),   #2 imagen
+    (110, 2530, 750, 3075),   #3 imagen
+
+    (930, 465, 1570, 1010),   #4 imagen
+    (930, 1455, 1570, 2000),   #5 imagen
+    (930, 2530, 1570, 3075),   #6 imagen
+
+    (1750, 465, 2390, 1010),   #7 imagen
+    (1750, 1455, 2390, 2000),  #8 imagen
+    (1750, 2530, 2390, 3075),  #9 imagen
 ]
+
+# +---------+---------+---------+
+# |         |         |         |
+# |   I1    |   I4    |   I7    |
+# |         |         |         |
+# +---------+---------+---------+
+# |         |         |         |
+# |   I2    |   I5    |   I8    |
+# |         |         |         |
+# +---------+---------+---------+
+# |         |         |         |
+# |   I3    |   I6    |   I9    |
+# |         |         |         |
+# +---------+---------+---------+
+
 
 # Recortar y guardar las imágenes individuales
 for i, (left, upper, right, lower) in enumerate(coords):
